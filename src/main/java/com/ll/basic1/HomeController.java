e8cf96ed0ec72303282973b0bdc0962cf68fc75f
kkambbak <yunsung9841@gmail.com>
2023-03-14 13:01:02 +0900

    RequestParam

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.basic1;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //스프링부트에게 이게 컨트롤러다 라고 알려줌
@@ -28,5 +29,14 @@ public class HomeController {
     @ResponseBody
     public int showIncrease(){
         return num++;
+    }
+
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    //int a 는 쿼리스트링에서 a,b 파라미터의 값을 얻은 후 정수화한 값이어야한다.
+    //쿼리스트링은 순서가 없다.
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
+        return a + b;
     }
 }
